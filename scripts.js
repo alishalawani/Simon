64c3deda52f82237be72cd4c38b46caf4bc37c74
alishalawani <alishalawani@gmail.com>
2020-07-13 15:02:17 -0500

    added a click event for the start button, and made the buttons glow when they are randomly chosen

--- scripts.js
+++ scripts.js
@@ -1,4 +1,40 @@
-// setting the first Round
+// Grabbing the buttons
+const cirlce = document.querySelector('.circle');
+const bluebutton = document.querySelector('.blue');
+const redButton = document.querySelector('.red');
+const yellowButton = document.querySelector('.yellow');
+const greenButton = document.querySelector('.green');
+
 let roundCount = 1;
 document.querySelector('.round-js').innerText = `Round ${roundCount}`;
 
+let startButton = document.querySelector('.start-button');
+
+startButton.addEventListener('click', handleStartButton);
+
+var rando = 1;
+function gameChoice() {
+	for (let i = 0; i < roundCount; i++) {
+		rando = Math.floor(Math.random() * 4) + 1;
+
+		if (rando == 1) {
+			bluebutton.classList.add('onBlue');
+			console.log('blue');
+		} else if (rando == 2) {
+			redButton.classList.add('onRed');
+			console.log('red');
+		} else if (rando == 3) {
+			yellowButton.classList.add('onYellow');
+			console.log('yellow');
+		} else {
+			greenButton.classList.add('onGreen');
+			console.log('green');
+		}
+	}
+}
+
+function handleStartButton(event) {
+	console.log('handleStartButton');
+
+	gameChoice();
+}
