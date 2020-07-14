f04b8d34e8ed8cb0d247be6b121d64cbdc6ba8da
alishalawani <alishalawani@gmail.com>
2020-07-13 23:17:01 -0500

    made the buttons glow one at a time and made sure no button was still glowing after the game finished it's sequence

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // Grabbing the buttons
 const cirlce = document.querySelector('.circle');
-const bluebutton = document.querySelector('.blue');
+const blueButton = document.querySelector('.blue');
 const redButton = document.querySelector('.red');
 const yellowButton = document.querySelector('.yellow');
 const greenButton = document.querySelector('.green');
 
-let roundCount = 1;
+let roundCount = 4;
 document.querySelector('.round-js').innerText = `Round ${roundCount}`;
 
 let startButton = document.querySelector('.start-button');
@@ -13,28 +13,78 @@ let startButton = document.querySelector('.start-button');
 startButton.addEventListener('click', handleStartButton);
 
 var rando = 1;
+let timeout = 2000;
+let timeoutTracker = timeout;
+let userSequence = [];
+let gameSequence = [];
+
 function gameChoice() {
 	for (let i = 0; i < roundCount; i++) {
 		rando = Math.floor(Math.random() * 4) + 1;
-
+		
 		if (rando == 1) {
-			bluebutton.classList.add('onBlue');
+			setTimeout(() => {
+				checkAndRemoveColors();
+				blueButton.classList.add('onBlue');
+			}, timeout);
+			checkAndRemoveColors();
+				blueButton.classList.add('onBlue');
+			//ADD THE CHOICE TO THE SEQUENCE
+			gameSequence.push(`${blueButton.dataset.color}`);
 			console.log('blue');
 		} else if (rando == 2) {
-			redButton.classList.add('onRed');
+			setTimeout(() => {
+				checkAndRemoveColors();
+				redButton.classList.add('onRed');
+			}, timeout);
+			//ADD THE CHOICE TO THE SEQUENCE
+			gameSequence.push(`${redButton.dataset.color}`);
 			console.log('red');
 		} else if (rando == 3) {
-			yellowButton.classList.add('onYellow');
+			setTimeout(() => {
+				checkAndRemoveColors();
+				yellowButton.classList.add('onYellow');
+			}, timeout);
+			//ADD THE CHOICE TO THE SEQUENCE
+			gameSequence.push(`${yellowButton.dataset.color}`);
 			console.log('yellow');
 		} else {
-			greenButton.classList.add('onGreen');
+			setTimeout(() => {
+				checkAndRemoveColors();
+				greenButton.classList.add('onGreen');
+			}, timeout);
+			//ADD THE CHOICE TO THE SEQUENCE
+			gameSequence.push(`${greenButton.dataset.color}`);
 			console.log('green');
 		}
+		
+		timeout += 1000;
+		setTimeout(checkAndRemoveColors, timeout);
 	}
+	timeout = timeoutTracker;
 }
-
+setTimeout(checkAndRemoveColors, timeout);
 function handleStartButton(event) {
 	console.log('handleStartButton');
 
 	gameChoice();
 }
+//makes sure only one button lights up at a time
+function checkAndRemoveColors() {
+	if (blueButton.classList.contains('onBlue')) {
+		blueButton.classList.remove('onBlue');
+	} else if (redButton.classList.contains('onRed')) {
+		redButton.classList.remove('onRed');
+	} else if (yellowButton.classList.contains('onYellow')) {
+		yellowButton.classList.remove('onYellow');
+	} else if (greenButton.classList.contains('onGreen')) {
+		greenButton.classList.remove('onGreen');
+	}
+}
+
+function delay (ms){
+	const startPoint = new Date().getTime();
+	while (new Date().getTime() - startPoint <= ms) {
+		/* wait */
+	}
+};
